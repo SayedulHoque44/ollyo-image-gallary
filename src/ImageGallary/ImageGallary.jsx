269c1735afad3a7a812244ee9d3e48d4c9610cf6
sayedulhoque44 <sayedulhoque3544@gmail.com>
2023-11-03 12:24:40 +0600

    is dragged or target is fetured added

--- src/ImageGallary/ImageGallary.jsx
+++ src/ImageGallary/ImageGallary.jsx
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
 import { ImCheckboxChecked } from "react-icons/im";
 import image1 from "../assets/images/image-1.webp";
+import image10 from "../assets/images/image-10.jpeg";
+import image11 from "../assets/images/image-11.jpeg";
 import image2 from "../assets/images/image-2.webp";
 import image3 from "../assets/images/image-3.webp";
 import image4 from "../assets/images/image-4.webp";
 import image5 from "../assets/images/image-5.webp";
 import image6 from "../assets/images/image-6.webp";
 import image7 from "../assets/images/image-7.webp";
+import image8 from "../assets/images/image-8.webp";
+import image9 from "../assets/images/image-9.webp";
 import Container from "../components/Container";
+import SingleImage from "./SingleImage/SingleImage";
 const ImageGallary = () => {
   const [images, setImages] = useState([
     { id: 1, src: image1, isFeatured: true },
@@ -17,11 +22,15 @@ const ImageGallary = () => {
     { id: 5, src: image5, isFeatured: false },
     { id: 6, src: image6, isFeatured: false },
     { id: 7, src: image7, isFeatured: false },
+    { id: 8, src: image8, isFeatured: false },
+    { id: 9, src: image9, isFeatured: false },
+    { id: 10, src: image10, isFeatured: false },
+    { id: 11, src: image11, isFeatured: false },
     // Add more images here
   ]);
-  const [selectedImages, setSelectedImages] = useState([]);
-  const [draggedImage, setDraggedImage] = useState(null);
+
   const [selectedImgesId, setSelectedImagesId] = useState([]);
+  const [draggedImage, setDraggedImage] = useState(null);
   //
   const handleFeatureImage = (imageId) => {
     const updatedImages = images.map((image) => {
@@ -37,133 +46,48 @@ const ImageGallary = () => {
   //
   const handleDeleteImages = () => {
     const updatedImages = images.filter(
-      (image) => !selectedImages.includes(image.id)
+      (image) => !selectedImgesId.includes(image.id)
     );
     setImages(updatedImages);
-    setSelectedImages([]);
-  };
-  //
-  const handleDragStart = (e, image) => {
-    setDraggedImage(image);
+    setSelectedImagesId([]);
   };
 
-  const handleDragOver = (e) => {
-    e.preventDefault();
-  };
-  //
-  const handleDrop = (e, targetImage) => {
-    if (draggedImage) {
-      const updatedImages = [...images];
-      const draggedIndex = images.findIndex(
-        (image) => image.id === draggedImage.id
-      );
-      const targetIndex = images.findIndex(
-        (image) => image.id === targetImage.id
-      );
-
-      [updatedImages[draggedIndex], updatedImages[targetIndex]] = [
-        updatedImages[targetIndex],
-        updatedImages[draggedIndex],
-      ];
-
-      setImages(updatedImages);
-      setDraggedImage(null);
-    }
-  };
-  //
-  const handleChecked = (e, selectedImgId) => {
-    const currentSelectedImagesId = [...selectedImgesId];
-    if (e.target.checked) {
-      currentSelectedImagesId.push(selectedImgId);
-      setSelectedImagesId(currentSelectedImagesId);
-    } else {
-      const updateSelectedImagesId = currentSelectedImagesId.filter(
-        (id) => id !== selectedImgId
-      );
-      setSelectedImagesId(updateSelectedImagesId);
-    }
-  };
-  //
-
-  const isCheked = (imageId) => {
-    let checked = false;
-    for (const id of selectedImgesId) {
-      if (id === imageId) {
-        checked = true;
-        break;
-      }
-    }
-
-    return checked;
-  };
-
-  //
   // console.log(selectedImgesId);
   return (
     <Container>
       <div className="shadow-lg border-2 border-gray-100 p-5">
-        {selectedImgesId.length > 0 && (
-          <div className="flex justify-between items-center">
-            <div className="flex gap-3 items-center">
-              <ImCheckboxChecked size={24} color="blue" />
-              <p className="text-2xl font-semibold">
-                {selectedImgesId.length} selected
-              </p>
-            </div>
-
-            <button
-              onClick={handleDeleteImages}
-              disabled={selectedImages.length === 0}
-              className=" bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600">
-              Delete Selected Images
-            </button>
+        <div
+          className={`flex justify-between ease-in-out duration-300 items-center ${
+            selectedImgesId.length > 0 ? "opacity-1" : "opacity-0"
+          }`}>
+          <div className="flex gap-3 items-center">
+            <ImCheckboxChecked size={24} color="blue" />
+            <p className="text-2xl font-semibold">
+              {selectedImgesId.length} selected
+            </p>
           </div>
-        )}
+
+          <button
+            onClick={handleDeleteImages}
+            className=" bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600">
+            Delete {selectedImgesId.length > 1 ? "files" : "file"}
+          </button>
+        </div>
 
         <div className="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4  gap-4  m-2 p-3 ">
           {images.map((image) => (
-            <div
+            //we can use context provider to ignor the props drilling
+            <SingleImage
               key={image.id}
-              className={`border p-2 group/item shadow-sm relative ${
-                image.isFeatured ? "lg:col-span-2 row-span-2" : ""
-              }`}
-              draggable
-              onDragStart={(e) => handleDragStart(e, image)}
-              onDragOver={handleDragOver}
-              onDrop={(e) => handleDrop(e, image)}>
-              <picture>
-                <div
-                  className={`absolute ${
-                    isCheked(image.id)
-                      ? "opacity-1 bg-white/60"
-                      : "group-hover/item:opacity-100 opacity-0 bg-black/25"
-                  }  ease-in-out duration-300   top-0 bottom-0 right-0 left-0 `}>
-                  <input
-                    type="checkbox"
-                    onChange={(e) => handleChecked(e, image.id)}
-                    name="select"
-                    id="select"
-                    className="m-3"
-                  />
-                </div>
-
-                <source srcSet={image.src} type="image/webp" />
-                <img
-                  src={image.src.replace(".webp", ".jpg")} // Provide a fallback image format (e.g., JPEG)
-                  alt={`Image ${image.id}`}
-                  className={`w-full ${
-                    selectedImages.includes(image.id)
-                      ? "border-4 border-blue-500"
-                      : ""
-                  }`}
-                />
-              </picture>
-              <div
-                className="bg-blue-500 text-white p-2 text-center cursor-pointer"
-                onClick={() => handleFeatureImage(image.id)}>
-                Set as Feature
-              </div>
-            </div>
+              image={image}
+              draggedImage={draggedImage}
+              setDraggedImage={setDraggedImage}
+              handleFeatureImage={handleFeatureImage}
+              setSelectedImagesId={setSelectedImagesId}
+              images={images}
+              setImages={setImages}
+              selectedImgesId={selectedImgesId}
+            />
           ))}
         </div>
       </div>
---
+++ src/ImageGallary/SingleImage/SingleImage.jsx
@@ -0,0 +1,136 @@
+import React from "react";
+
+const SingleImage = ({
+  image,
+  setImages,
+  images,
+  setSelectedImagesId,
+  handleFeatureImage,
+  selectedImgesId,
+  setDraggedImage,
+  draggedImage,
+}) => {
+  // Its call when the image/div drag started
+  const handleDragStart = (e, image) => {
+    setDraggedImage(image);
+  };
+
+  // its for unnessesary event when object draged
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  // Its call when drop the image/div
+  const handleDrop = (e, targetImage) => {
+    if (draggedImage) {
+      const updatedImages = [...images];
+
+      const draggedIndex = images.findIndex(
+        (image) => image.id === draggedImage.id
+      );
+
+      const targetIndex = images.findIndex(
+        (image) => image.id === targetImage.id
+      );
+
+      //is target image is feature images
+      if (targetImage.isFeatured) {
+        updatedImages[draggedIndex].isFeatured = true;
+        updatedImages[targetIndex].isFeatured = false;
+        [updatedImages[draggedIndex], updatedImages[targetIndex]] = [
+          updatedImages[targetIndex],
+          updatedImages[draggedIndex],
+        ];
+        setImages(updatedImages);
+        setDraggedImage(null);
+        return;
+      } else if (updatedImages[draggedIndex].isFeatured) {
+        updatedImages[draggedIndex].isFeatured = false;
+        updatedImages[targetIndex].isFeatured = true;
+        [updatedImages[draggedIndex], updatedImages[targetIndex]] = [
+          updatedImages[targetIndex],
+          updatedImages[draggedIndex],
+        ];
+        setImages(updatedImages);
+        setDraggedImage(null);
+        return;
+      } else {
+        [updatedImages[draggedIndex], updatedImages[targetIndex]] = [
+          updatedImages[targetIndex],
+          updatedImages[draggedIndex],
+        ];
+        setImages(updatedImages);
+        setDraggedImage(null);
+      }
+    }
+  };
+
+  // Make image selected
+  const handleChecked = (e, selectedImgId) => {
+    const currentSelectedImagesId = [...selectedImgesId];
+    if (e.target.checked) {
+      currentSelectedImagesId.push(selectedImgId);
+      setSelectedImagesId(currentSelectedImagesId);
+    } else {
+      const updateSelectedImagesId = currentSelectedImagesId.filter(
+        (id) => id !== selectedImgId
+      );
+      setSelectedImagesId(updateSelectedImagesId);
+    }
+  };
+
+  // check is image selected or not
+  const isCheked = (imageId) => {
+    let checked = false;
+    for (const id of selectedImgesId) {
+      if (id === imageId) {
+        checked = true;
+        break;
+      }
+    }
+    return checked;
+  };
+  //
+  return (
+    <div
+      key={image.id}
+      className={`border p-2 group/item shadow-sm relative cursor-pointer ${
+        image.isFeatured ? "lg:col-span-2 row-span-2" : ""
+      }`}
+      draggable
+      onDragStart={(e) => handleDragStart(e, image)}
+      onDragOver={handleDragOver}
+      onDrop={(e) => handleDrop(e, image)}>
+      <picture>
+        <div
+          className={`absolute ${
+            isCheked(image.id)
+              ? "opacity-1 bg-white/60"
+              : "group-hover/item:opacity-100 opacity-0 bg-black/25"
+          }  ease-in-out duration-300   top-0 bottom-0 right-0 left-0 `}>
+          <input
+            type="checkbox"
+            onChange={(e) => handleChecked(e, image.id)}
+            name="select"
+            id="select"
+            className="m-3 h-5 w-5"
+          />
+          <div
+            className="bg-blue-500 absolute bottom-0 right-0 left-0 text-white p-2 text-center cursor-pointer"
+            onClick={() => handleFeatureImage(image.id)}>
+            Set as Feature
+          </div>
+        </div>
+
+        <source srcSet={image.src} type="image/webp" />
+        <img
+          src={image.src.replace(".webp", ".jpg")} // Provide a fallback image format (e.g., JPEG)
+          alt={`Image ${image.id}`}
+          className={`w-full`}
+        />
+      </picture>
+    </div>
+  );
+};
+
+export default SingleImage;
